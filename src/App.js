c380a9b2c125001dd72cbd558e533f13503072aa
Luke Schlangen <lukeschlangen@gmail.com>
2018-02-28 14:39:47 -0600

    add required url parameter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Animals from './pages/Animals/Animals';
+import Details from './pages/Details/Details';
 
 class App extends Component {
   render() {
@@ -22,6 +23,9 @@ class App extends Component {
               <li>
                 <Link to="/animals">Animals</Link>
               </li>
+              <li>
+                <Link to="/details">Details</Link>
+              </li>
             </ul>
 
             <hr />
@@ -29,6 +33,9 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/animals" component={Animals} />
+
+            {/* Required Parameter */}
+            <Route path="/details/:id" component={Details} />
           </div>
         </Router>
       </div>
